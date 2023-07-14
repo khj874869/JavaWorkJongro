0ea27635e58036241241a232cbaf3ba77d1c6a74
khj874869 <134674147+khj874869@users.noreply.github.com>
2023-07-14 17:50:52 +0900

    [ADd]

--- src/com/KH/day15/swing/exercise/Exercise_DialogSetText.java
+++ src/com/KH/day15/swing/exercise/Exercise_DialogSetText.java
@@ -12,9 +12,45 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
+import com.kh.day15.swing.exercise.MyModalDialog;
+
 import images.Exercise_DialogSetText;
 
 class MyModalDialog extends JDialog {
+	private JTextField field;
+	private JButton btn;
+	public MyModalDialog() {}
+	public MyModalDialog(JFrame frame, String title, JButton jBtn) {
+		super(frame, title);
+		setLayout(new FlowLayout());
+		field = new JTextField(10);
+		add(field);
+		btn = new JButton("OK");
+		btn.addActionListener(new ActionListener() {	// OK 버튼 눌렸을때
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);						// 다이얼로그 사라짐
+				if(field.getText().length() == 0) return;
+				else jBtn.setText(field.getText());
+				field.setText("");		// 다이얼로그 호출될 때 필드를 비워줌
+			}
+		});
+		add(btn);
+		setSize(200, 100);
+	}
+	
+	public void clear() {
+		field.setText("");
+	}
+	
+//	public String getInput() {
+//		if(field.getText().length() == 0) return null;
+//		else return field.getText();
+//	}
+}
+
+
+public class Exercise_DialogSetText extends JFrame{
 	private MyModalDialog dialog;
 	public Exercise_DialogSetText() {
 		super("다이얼로그 텍스트 셋팅 예제");
@@ -26,6 +62,7 @@ class MyModalDialog extends JDialog {
 		btn.addActionListener(new ActionListener() {	// 버튼 누르면 다이얼로그 띄어줌
 			@Override
 			public void actionPerformed(ActionEvent e) {
+//				dialog.clear();
 				dialog.setVisible(true);
 //				String text = dialog.getInput();
 //				if(text == null) return;
@@ -44,6 +81,4 @@ class MyModalDialog extends JDialog {
 	public static void main(String[] args) {
 		new Exercise_DialogSetText();
 	}
-
-
 }
